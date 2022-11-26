54f7216715cdeeb0423ad58a0627afa4097bec6d
kiselev.m <bigunclemax@gmail.com>
2022-11-26 13:41:30 +0300

    vbfeditor: initial version

---
+++ README.md
@@ -0,0 +1,46 @@
+# VbfEditor
+
+Open source library and tool for repacking VBF files.
+VBF(Volvo Binary File) format is widely used in automotive software for Ford and Volvo.   
+
+## Usage
+```
+Usage:
+  vbfeditor [OPTION...] positional parameters
+
+  -p, --pack        Pack VBF file
+  -u, --unpack      Unpack VBF file
+  -I, --info        Show info about VBF file
+  -i, --input arg   Input file
+  -o, --output arg  Output directory (default: "")
+  -v, --version     Print version
+  -h, --help        Print help
+```
+
+### Show VBF info
+Use `-I` option to display info about VBF file sections:  
+```
+vbfeditor -I path_to.vbf 
+Found 2 sections
+  # |    Offset    |  Start addr  |  Length 
+  0 |  0x000004e7  |  0x00004000  |  0x0009ffa0 (655264)
+  1 |  0x000a0491  |  0x000b6890  |  0x00000f10 (3856)
+```
+
+### Unpack VBF
+
+Use `-u` option to extract VBF sections to binary files:  
+`vbfeditor -u path_to.vbf -o unpacked/vbf/dir/`  
+
+Extracted content will be placed to specified folder:
+```
+unpacked/vbf/dir/
+├── path_to.vbf_ascii_head.txt
+├── path_to.vbf_config.json
+├── path_to.vbf_section_1_4000_9ffa0.bin
+└── path_to.vbf_section_2_b6890_f10.bin
+```
+
+### Pack VBF 
+Use `-p` option to repack modified binary sections to VBF container:  
+`vbfeditor -p vbf_conifg -o path/to/repacked.vbf`
---
+++ VbfEditor/main.cpp
@@ -0,0 +1,77 @@
+#include <winsock2.h>
+
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <list>
+#include <regex>
+
+#include <cxxopts.hpp>
+#include "VbfeditConfig.h"
+#include "VbfFile.h"
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    try {
+        bool pack = false;
+        bool unpack = false;
+
+        cxxopts::Options options("vbfeditor", "Tool for repacking VBF files");
+        options.add_options()
+                ("p,pack","Pack VBF file", cxxopts::value<bool>(pack))
+                ("u,unpack","Unpack VBF file", cxxopts::value<bool>(unpack))
+                ("i,input","Input file", cxxopts::value<string>())
+                ("o,output","Output directory", cxxopts::value<string>()->default_value(""))
+                ("v,version","Print version")
+                ("h,help","Print help");
+
+        options.parse_positional({"input"});
+        options.show_positional_help();
+
+        auto result = options.parse(argc, argv);
+
+        if(result.arguments().empty() || result.count("help")){
+            cout << options.help() << std::endl;
+            return 0;
+        }
+
+        if(result.count("version")){
+            cout << "vbfeditor version: "
+            << Vbfedit_VERSION_MAJOR
+            << Vbfedit_VERSION_MINOR
+            << std::endl;
+            return 0;
+        }
+
+        if(!result.count("input")){
+            cout << "Please, specify input file" << std::endl;
+            return 0;
+        }
+
+        if (pack){
+            cout << "Sry, pack currently not supported" << endl;
+            return 0;
+        }
+
+        if(unpack){
+            VbfFile vbf;
+            vbf.OpenFile(result["input"].as<string>());
+            if(vbf.IsOpen())
+                vbf.Export(result["output"].as<string>());
+            else
+                cout << "open error" << endl;
+
+            return 0;
+        }
+
+        cout << options.help() << std::endl;
+
+    } catch (const cxxopts::OptionException& e){
+        cout << "error parsing options: " << e.what() << endl;
+    }
+
+    return 0;
+}
