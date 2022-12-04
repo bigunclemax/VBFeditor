4c3696a5c2d650f2c577d14a18bfa36da352a688
kiselev.m <bigunclemax@gmail.com>
2022-12-04 14:29:12 +0300

    vbflib: use vector containers for LZSS operations

--- VbfEditor/lib/lzss.cpp
+++ VbfEditor/lib/lzss.cpp
@@ -1,86 +1,97 @@
 /* LZSS encoder-decoder (Haruhiko Okumura; public domain) */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <vector>
+#include <cstdint>
+#include <cstdio>
 
 #define EI 10  /* typically 10..13 */
 #define EJ  4  /* typically 4..5 */
 #define P   1  /* If match length <= P then output one character */
 #define N (1 << EI)  /* buffer size */
-#define F ((1 << EJ) + 1)  /* lookahead buffer size */
+#define F ((1 << EJ) + P)  /* lookahead buffer size */
 
-int bit_buffer = 0, bit_mask = 128;
-unsigned long codecount = 0, textcount = 0;
-unsigned char buffer[N * 2];
-FILE *infile, *outfile;
+constexpr int ROUNDUP_DEC_RATIO = ((F * 8) + ((EI + EJ) - 1)) / (EI + EJ);
+constexpr int EXPECT_DEC_RATIO = (ROUNDUP_DEC_RATIO + (2 - 1)) / 2;
 
-void error(void)
+using std::vector;
+
+struct enc_ctx {
+    int bit_buffer = 0;
+    int bit_mask = 128;
+};
+
+struct dec_ctx {
+    int idx = 0;
+    int buf = 0;
+    int mask = 0;
+};
+
+void putbit1(enc_ctx &ctx, vector<uint8_t> &data)
 {
-    printf("Output error\n");  exit(1);
-}
-
-void putbit1(void)
-{
-    bit_buffer |= bit_mask;
-    if ((bit_mask >>= 1) == 0) {
-        if (fputc(bit_buffer, outfile) == EOF) error();
-        bit_buffer = 0;  bit_mask = 128;  codecount++;
+    ctx.bit_buffer |= ctx.bit_mask;
+    if ((ctx.bit_mask >>= 1) == 0) {
+        data.push_back(ctx.bit_buffer);
+        ctx.bit_buffer = 0;  ctx.bit_mask = 128;
     }
 }
 
-void putbit0(void)
+void putbit0(enc_ctx &ctx, vector<uint8_t> &data)
 {
-    if ((bit_mask >>= 1) == 0) {
-        if (fputc(bit_buffer, outfile) == EOF) error();
-        bit_buffer = 0;  bit_mask = 128;  codecount++;
+    if ((ctx.bit_mask >>= 1) == 0) {
+        data.push_back(ctx.bit_buffer);
+        ctx.bit_buffer = 0;  ctx.bit_mask = 128;
     }
 }
 
-void flush_bit_buffer(void)
+void flush_bit_buffer(enc_ctx &ctx, vector<uint8_t> &data)
 {
-    if (bit_mask != 128) {
-        if (fputc(bit_buffer, outfile) == EOF) error();
-        codecount++;
+    if (ctx.bit_mask != 128) {
+        data.push_back(ctx.bit_buffer);
     }
 }
 
-void output1(int c)
+void output1(enc_ctx &ctx, vector<uint8_t> &data, int c)
 {
     int mask;
 
-    putbit1();
+    putbit1(ctx, data);
     mask = 256;
     while (mask >>= 1) {
-        if (c & mask) putbit1();
-        else putbit0();
+        if (c & mask) putbit1(ctx, data);
+        else putbit0(ctx, data);
     }
 }
 
-void output2(int x, int y)
+void output2(enc_ctx &ctx, vector<uint8_t> &data, int x, int y)
 {
     int mask;
 
-    putbit0();
+    putbit0(ctx, data);
     mask = N;
     while (mask >>= 1) {
-        if (x & mask) putbit1();
-        else putbit0();
+        if (x & mask) putbit1(ctx, data);
+        else putbit0(ctx, data);
     }
     mask = (1 << EJ);
     while (mask >>= 1) {
-        if (y & mask) putbit1();
-        else putbit0();
+        if (y & mask) putbit1(ctx, data);
+        else putbit0(ctx, data);
     }
 }
 
-void encode(void)
+vector<uint8_t> encode(const vector<uint8_t> &data)
 {
+    unsigned char buffer[N * 2];
+    vector<uint8_t> cdata;
+    cdata.reserve(data.size());
+    enc_ctx ctx{};
+    int idx = 0;
     int i, j, f1, x, y, r, s, bufferend, c;
 
     for (i = 0; i < N - F; i++) buffer[i] = '\0';
     for (i = N - F; i < N * 2; i++) {
-        if ((c = fgetc(infile)) == EOF) break;
-        buffer[i] = c;  textcount++;
+        if (idx >= data.size()) break;
+        buffer[i] = data[idx++];
     }
     bufferend = i;  r = N - F;  s = 0;
     while (r < bufferend) {
@@ -99,56 +110,62 @@ void encode(void)
             x -= (N - F);
         x++;
 
-        if (y <= P) {  output1(c);  }
-        else output2(x & (N - 1), y - 2);
+        if (y <= P) {  output1(ctx, cdata, c);  }
+        else output2(ctx, cdata, x & (N - 1), y - 2);
         r += y;  s += y;
         if (r >= N * 2 - F) {
             for (i = 0; i < N; i++) buffer[i] = buffer[i + N];
             bufferend -= N;  r -= N;  s -= N;
             while (bufferend < N * 2) {
-                if ((c = fgetc(infile)) == EOF) break;
-                buffer[bufferend++] = c;  textcount++;
+                if (idx >= data.size()) break;
+                buffer[bufferend++] = data[idx++];
             }
         }
     }
-    flush_bit_buffer();
-    printf("text:  %ld bytes\n", textcount);
-    printf("code:  %ld bytes (%ld%%)\n",
-        codecount, (codecount * 100) / textcount);
+    flush_bit_buffer(ctx, cdata);
+
+    return cdata;
 }
 
-int getbit(int n) /* get n bits */
+int getbit(dec_ctx &ctx, const vector<uint8_t> &data, int n) /* get n bits */
 {
     int i, x;
-    static int buf, mask = 0;
-    
+
     x = 0;
     for (i = 0; i < n; i++) {
-        if (mask == 0) {
-            if ((buf = fgetc(infile)) == EOF) return EOF;
-            mask = 128;
+        if (ctx.mask == 0) {
+
+            if (ctx.idx >= data.size())
+                return EOF;
+
+            ctx.buf = data[ctx.idx++];
+            ctx.mask = 128;
         }
         x <<= 1;
-        if (buf & mask) x++;
-        mask >>= 1;
+        if (ctx.buf & ctx.mask) x++;
+        ctx.mask >>= 1;
     }
     return x;
 }
 
-void decode(void)
+vector<uint8_t> decode(const vector<uint8_t> &cdata)
 {
+    unsigned char buffer[N * 2];
+    dec_ctx ctx{};
+    vector<uint8_t> data;
+    data.reserve(cdata.size() * EXPECT_DEC_RATIO);
     int i, j, k, r, c;
 
     r = 0;
 
-    while ((c = getbit(1)) != EOF) {
+    while ((c = getbit(ctx, cdata, 1)) != EOF) {
         if (c) {
-            if ((c = getbit(8)) == EOF) break;
-            fputc(c, outfile);
+            if ((c = getbit(ctx, cdata, 8)) == EOF) break;
+            data.push_back(c);
             buffer[r++] = c;  r &= (N - 1);
         } else {
-            if ((i = getbit(EI)) == EOF) break;
-            if ((j = getbit(EJ)) == EOF) break;
+            if ((i = getbit(ctx, cdata, EI)) == EOF) break;
+            if ((j = getbit(ctx, cdata, EJ)) == EOF) break;
 
             if (i == 0)
                 break;
@@ -157,9 +174,13 @@ void decode(void)
 
             for (k = 0; k <= j + 1; k++) {
                 c = buffer[(i + k) & (N - 1)];
-                fputc(c, outfile);
+                data.push_back(c);
                 buffer[r++] = c;  r &= (N - 1);
             }
         }
     }
+
+    data.shrink_to_fit();
+
+    return data;
 }
